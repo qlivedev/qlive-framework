9770428e9e777c539137e4dbd3bdc59858d37dcd
Sven Helmberger <fforw@gmx.de>
2026-08-31 16:51:55 +0200

    Fix GraphQLQueryTypingServiceTest
    This reverts commit a0a77083f6468942b4cc6764ef15df002ef66c58.

--- qlive/src/test/resources/test-ts-root/sub/Q_Test.template.ts
+++ qlive/src/test/resources/test-ts-root/sub/Q_Test.template.ts
@@ -1,8 +1,8 @@
-import {GraphQLQuery,} from "@quinscape/qlive-js";
+import { GraphQLQuery, } from "@quinscape/qlive-js";
 
 export const Q_Test = new GraphQLQuery<any>(
     // language=GraphQL
-        `query Q_Test($config: QueryConfig!) {
+    `query Q_Test($config: QueryConfig!) {
         queryTestFooDocument(config: $config) {
             type
             config

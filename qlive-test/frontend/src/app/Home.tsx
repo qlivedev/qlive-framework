7d8a266958d11e5fa338fd35ea666c68f6ca0b51
Sven Helmberger <fforw@gmx.de>
2026-09-04 16:44:23 +0200

    Home: Reduce view
    Just dump the injected foos

--- qlive-test/frontend/src/app/Home.tsx
+++ qlive-test/frontend/src/app/Home.tsx
@@ -8,35 +8,19 @@ interface HomeProps
     foos: Q_FooResult;
 }
 
-export default function Home({foos = inject(Q_Foo, {})}: HomeProps) {
-    const filter =
-        field("name").eq(value("Foo #1")).or(field("owner.login").eq(value("admin")))
+export default function Home({foos = inject(Q_Foo)}: HomeProps) {
+
+    // const filter =
+    //     field("name").eq(value("Foo #1")).or(field("owner.login").eq(value("admin")))
 
     return (
         <div>
             <h1>Home</h1>
 
-            <dl>
-                <dt>Foo</dt>
-                <dd>
-                    <pre>
-                        {
-                            JSON.stringify({foos}, null, 4)
-                        }
-                    </pre>
-                </dd>
-                <dt>Filter</dt>
-                <dd>
-                    <pre>
-                    {
-                        JSON.stringify(filter, null, 4)
-                    }
-                    </pre>
-                </dd>
-
-
-            </dl>
             <pre>
+            {
+                JSON.stringify(foos, null, 4)
+            }
             </pre>
         </div>
     );

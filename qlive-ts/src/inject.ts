747777fdf75eb6efd7a7f1b187a6563effe1fc51
Sven Helmberger <fforw@gmx.de>
2026-09-04 17:10:50 +0200

    Implement inject function
    Replace our placeholder contents with the real thing.
    
    We now get the data as Injection, so we need to the actual value from
    that. We check if the type is QueryDocument derrived and register the
    injected query if it is.
    
    We only use queries with one query method for injection, so we just
    grab the first result value as payload.

--- qlive-ts/src/inject.ts
+++ qlive-ts/src/inject.ts
@@ -1,6 +1,8 @@
 import {GraphQLQuery} from "./GraphQLQuery";
 import {GraphQLParams} from "./util/graphql";
-import {field, value} from "./FilterDSL";
+import data from "./data";
+import {isQueryDocumentType} from "./type-utils";
+import {QueryDocument} from "./QueryDocument";
 
 /**
  * Causes the injection of the given GraphQL result.
@@ -10,15 +12,52 @@ import {field, value} from "./FilterDSL";
  *
  * @returns injected Value
  */
-export default function inject<T>(query: GraphQLQuery<T>, params: GraphQLParams): T {
-    const data = {
-        config: {
-            offset: 0,
-            pageSize: 20,
-            condition: field("name").eq(value("John")),
-            sortFields: ["name"]
-        },
-        rows: []
+
+export type InjectParams = GraphQLParams & {
+    /**
+     * Used to disambiguate query results when the same named query gets used multiple times.
+     */
+    __id?: string
+}
+
+function getFirstValue(result: any)
+{
+    for (let key in result)
+    {
+        if (result.hasOwnProperty(key))
+        {
+            return result[key];
+        }
     }
-    return data as T;
+    return null;
+}
+
+/**
+ * Injects the data from the given query with the given optional params.
+ *
+ * The actual work for the data injection has already happened on the server side by the time this method actually
+ * gets called. We use static analysis to detect the inject calls in the code base and prepare them before sending
+ * each view.
+ *
+ * To receive the data, we need a injection id. Normally this is the query name as defined within the query definition.
+ * If you need to inject the same query twice for the same view, at least one of the injections needs to provide a
+ * __id parameter to disambiguate.
+ *
+ * @param query     GraphQLQuery
+ * @param params    GraphQLParams including __id
+ */
+export default function inject<T>(query: GraphQLQuery<T>, params: InjectParams = {}): T
+{
+    const { __id } = params
+
+    const injectionId = __id || query.queryName;
+    const injection = data(injectionId);
+    const result = injection.value;
+
+    if (isQueryDocumentType(injection.type))
+    {
+        query.register(result)
+    }
+    
+    return getFirstValue(result);
 }

3e6447c6bcd76b6ac58731faa8c6d121fe02e45a
Sven Helmberger <fforw@gmx.de>
2026-09-05 18:16:01 +0200

    Separate QueryDocument handling for Foo
    We don't try to use the not working service but just prepare random test
    data.
    
    Schema stays largely the same except for a comment on the
    queryFooDocument.

--- qlive-test/src/main/java/com/dataciders/qlivetest/runtime/logic/QueryLogic.java
+++ qlive-test/src/main/java/com/dataciders/qlivetest/runtime/logic/QueryLogic.java
@@ -11,11 +11,19 @@ import com.dataciders.qlivetest.domain.tables.pojos.Bar;
 import com.dataciders.qlivetest.domain.tables.pojos.Baz;
 import com.dataciders.qlivetest.domain.tables.pojos.Foo;
 import com.dataciders.qlivetest.domain.tables.pojos.FooType;
+import de.quinscape.domainql.fetcher.FetcherContext;
+import de.quinscape.domainql.jooq.GeneratedDomainObject;
 import graphql.schema.DataFetchingEnvironment;
 import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.UUID;
 
 @GraphQLLogic
 public class QueryLogic
@@ -42,7 +50,6 @@ public class QueryLogic
             namePattern = "query*Document",
             typeNamePattern = "*Document",
             types = {
-                Foo.class,
                 FooType.class,
                 Bar.class,
                 Baz.class,
@@ -60,6 +67,55 @@ public class QueryLogic
         return queryDocumentService.buildQuery(type, env, config)
             .selectByFilter(true)
             .execute();
+    }
+
+
+    // Test method querying [T]
+    @GraphQLQuery
+    public <T> @NotNull QueryDocument<T> testQueryDocument(
+        @GraphQLTypeParam(
+            namePattern = "query*Document",
+            typeNamePattern = "*Document",
+            types = {
+                Foo.class
+            }
+        )
+        Class<T> type,
+        DataFetchingEnvironment env,
+        @NotNull QueryConfig config
+    )
+    {
+        final QueryDocument<Foo> doc = new QueryDocument<>(Foo.class);
+        if (config == null)
+        {
+            config = new QueryConfig();
+            config.setPageSize(1);
+
+        }
+        doc.setConfig(config);
+        final ArrayList<Foo> rows = new ArrayList<>();
+        final Foo foo = new Foo();
+
+        int rnd = (int) Math.round(Math.random() * 100);
+
+        foo.setId(UUID.randomUUID().toString());
+        foo.setName("Foo #" + rnd);
+        foo.setNum(rnd);
+        foo.setDescription("Desc for Foo #" + rnd);
+        foo.setOwnerId("d7df0f2c-9aa8-4845-b2bf-1d02abd3666e");
+        final FetcherContext fetcherContext = new FetcherContext();
+        final AppUser user = new AppUser();
+        user.setId("d7df0f2c-9aa8-4845-b2bf-1d02abd3666e");
+        user.setLogin("admin");
+        user.setCreated(Timestamp.from(Instant.now()));
+        foo.provideFetcherContext(fetcherContext);
+
+        rows.add(foo);
+
+        doc.setRows(rows);
+        doc.setRowCount(1);
+
+        return (QueryDocument<T>) doc;
     }
 
 //    @GraphQLQuery
